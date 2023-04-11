84fe69e471e92ccb195cd91fb9ffcec14e84829b
QUOCHIEU69 <ngoquochieu2002@gmail.com>
2023-04-11 17:29:58 +0700

    bai1

---
+++ BTVN/tcpclient.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+// Client nhập số từ bàn phím và gửi sang server
+
+int main(int argc, char *argv[])
+{
+    char ip[9];
+    strcpy(ip, argv[1]);
+    int port = atoi(argv[2]);
+    printf("%s\n", ip);
+    printf("%d\n", port);
+
+    int client = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(ip);
+    addr.sin_port = htons(port);
+
+
+    int ret = connect(client, (struct sockaddr *)&addr, sizeof(addr));
+    if (ret == -1)
+    {
+        perror("connect() failed");
+        return 1;
+    }
+
+    char buf[256];
+    while (1)
+    {
+        printf("Enter string:");
+        fgets(buf, sizeof(buf), stdin);
+
+        send(client, buf, strlen(buf), 0);
+
+        if (strncmp(buf, "exit", 4) == 0)
+            break;
+    }
+
+    close(client);
+}
